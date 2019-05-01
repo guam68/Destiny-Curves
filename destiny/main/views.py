054169dfef0ed2bfe407d971b64e53267c3d7a51
guam68 <guam_68@hotmail.com>
2019-05-01 14:32:31 -0700

    assign proper dice value to cards

--- destiny/main/views.py
+++ destiny/main/views.py
@@ -12,21 +12,38 @@ def index(request):
 def get_decks(request):
     url = 'https://swdestinydb.com/api/public/decklist/'
     data = json.loads(request.body)
-    user_deck = requests.get(url + data['deck_id']).json()
-    opp_deck = requests.get(url + data['opp_deck']).json()
+    user_deck = requests.get(url + data['deck_id'].strip()).json()
+    opp_deck = requests.get(url + data['opp_deck'].strip()).json()
     user_char_arr = []
-    opp_char_arr = []
+    opp_char_arr = {} 
 
     user_chars = user_deck['characters']
     opp_chars = opp_deck['characters']
 
+    print(opp_chars)
+    i = 0
     for char in opp_chars:
-        print(char)
-        opp_char_arr += Card.objects.filter(id = char)
+        if opp_chars[char]['quantity'] == 1:
+            opp_char_arr [i] = { 
+                'card': Card.objects.filter(id = char)[0],
+                'dice': opp_chars[char]['dice']
+            }
+            i += 1
+            print(char)
+        else:
+            for copy in range(opp_chars[char]['quantity']):
+                opp_char_arr[i] = { 
+                    'card': Card.objects.filter(id = char)[0],
+                    'dice': 1
+                }
+                i += 1
+            
 
     print(opp_char_arr)
 
-    print(opp_char_arr[0].calc_dmg())
+    print(opp_char_arr[0]['card'].calc_dmg())
+
+    return
 
 
 
