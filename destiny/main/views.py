b08bc673a4eb00e400269c0861f06238ccebc8de
guam68 <guam_68@hotmail.com>
2019-05-07 16:02:39 -0700

    pre-priority graph working

--- destiny/main/views.py
+++ destiny/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from .models import Card
 import requests
 import json
@@ -14,37 +15,37 @@ def get_decks(request):
     data = json.loads(request.body)
     user_deck = requests.get(url + data['deck_id'].strip()).json()
     opp_deck = requests.get(url + data['opp_deck'].strip()).json()
-    user_char_arr = []
-    opp_char_arr = {} 
 
     user_chars = user_deck['characters']
     opp_chars = opp_deck['characters']
 
-    print(opp_chars)
+    user_char_dict = assign_characters(user_chars)
+    opp_char_dict = assign_characters(opp_chars)
+
+    return JsonResponse({'user': user_char_dict, 'opp': opp_char_dict})
+
+
+def assign_characters(chars):
+    char_dict = {}
     i = 0
-    for char in opp_chars:
-        if opp_chars[char]['quantity'] == 1:
-            opp_char_arr [i] = { 
-                'card': Card.objects.filter(id = char)[0],
-                'dice': opp_chars[char]['dice']
+    for char in chars:
+        if chars[char]['quantity'] == 1:
+            card = Card.objects.filter(id = char)[0]
+            char_dict[i] = { 
+                'card': model_to_dict(card),
+                'dice': chars[char]['dice'],
+                'dice_dmg': card.calc_dmg()
             }
             i += 1
-            print(char)
         else:
-            for copy in range(opp_chars[char]['quantity']):
-                opp_char_arr[i] = { 
-                    'card': Card.objects.filter(id = char)[0],
-                    'dice': 1
+            for copy in range(chars[char]['quantity']):
+                card = Card.objects.filter(id = char)[0]
+                char_dict[i] = { 
+                    'card': model_to_dict(Card.objects.filter(id = char)[0]),
+                    'dice': 1,
+                    'dice_dmg': card.calc_dmg()
                 }
                 i += 1
-            
-
-    print(opp_char_arr)
-
-    print(opp_char_arr[0]['card'].calc_dmg())
-
-    return
-
-
-
+    
+    return char_dict
 
